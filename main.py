42d108c94fd5d8a72700e8733d51f13cbf9dae3b
Codertjay <codertjay@gmail.com>
2022-04-03 21:43:52 +0100

    kk

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 import asyncio
 import datetime
+import time
 
 from decouple import config
 from web3 import Web3
 
-loop = asyncio.get_event_loop()
 x = datetime.datetime.now()
+
+loop = asyncio.get_event_loop()
 
 
 async def send_eth(value, gasPrice):
     web3 = Web3(Web3.HTTPProvider(config(F'ALCHEMY_DAY_{x.day}')))
-    while True:
-        try:
-            if web3.eth.get_balance(config('FROM_ACCOUNT')) > 286982993542500:
-                balance = web3.eth.get_balance(config('FROM_ACCOUNT'))
-                web3 = Web3(Web3.HTTPProvider("https://rpc.ethermine.org/"))
-                web3.eth.sendRawTransaction(
-                    web3.eth.account.signTransaction({
-                        'chainId': 1,
-                        'nonce': web3.eth.getTransactionCount(config('FROM_ACCOUNT'), 'pending'),
-                        'to': config('TO_ACCOUNT'),
-                        'value': int(balance * value),
-                        'gas': 21000,
-                        'gasPrice': int(balance * gasPrice / 21000)
-                    },
-                        config('PRIVATE_KEY')).rawTransaction)
-        except:
+    try:
+        if web3.eth.get_balance(config('FROM_ACCOUNT')) > 286982993542500:
             balance = web3.eth.get_balance(config('FROM_ACCOUNT'))
+            web3 = Web3(Web3.HTTPProvider("https://rpc.ethermine.org/"))
             web3.eth.sendRawTransaction(
                 web3.eth.account.signTransaction({
                     'chainId': 1,
@@ -37,7 +26,20 @@ async def send_eth(value, gasPrice):
                     'gasPrice': int(balance * gasPrice / 21000)
                 },
                     config('PRIVATE_KEY')).rawTransaction)
-
+    except:
+        balance = web3.eth.get_balance(config('FROM_ACCOUNT'))
+        web3.eth.sendRawTransaction(
+            web3.eth.account.signTransaction({
+                'chainId': 1,
+                'nonce': web3.eth.getTransactionCount(config('FROM_ACCOUNT'), 'pending'),
+                'to': config('TO_ACCOUNT'),
+                'value': int(balance * value),
+                'gas': 21000,
+                'gasPrice': int(balance * gasPrice / 21000)
+            },
+                config('PRIVATE_KEY')).rawTransaction)
+    print("hello")
+    time.sleep(5)
 
 async def main(loop):
     while True:
@@ -50,6 +52,7 @@ async def main(loop):
         loop.create_task(send_eth(value=0.6, gasPrice=0.3))
         loop.create_task(send_eth(value=0.7, gasPrice=0.2))
         loop.create_task(send_eth(value=0.8, gasPrice=0.1))
+        time.sleep(1)
 
 
 try:
