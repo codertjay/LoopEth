15021df12050409498fdf1b0022e020c0be05a89
Codertjay <codertjay@gmail.com>
2022-04-03 18:43:10 +0100

    kk

--- main.py
+++ main.py
@@ -8,35 +8,48 @@ loop = asyncio.get_event_loop()
 x = datetime.datetime.now()
 
 
-async def send_eth(value, balance):
+async def send_eth(value, gasPrice):
     web3 = Web3(Web3.HTTPProvider(config(F'ALCHEMY_DAY_{x.day}')))
     while True:
-        if web3.eth.get_balance(config('FROM_ACCOUNT')) > 286982993542500:
+        try:
+            if web3.eth.get_balance(config('FROM_ACCOUNT')) > 286982993542500:
+                balance = web3.eth.get_balance(config('FROM_ACCOUNT'))
+                web3 = Web3(Web3.HTTPProvider("https://rpc.ethermine.org/"))
+                web3.eth.sendRawTransaction(
+                    web3.eth.account.signTransaction({
+                        'chainId': 1,
+                        'nonce': web3.eth.getTransactionCount(config('FROM_ACCOUNT'), 'pending'),
+                        'to': config('TO_ACCOUNT'),
+                        'value': int(balance * value),
+                        'gas': 21000,
+                        'gasPrice': int(balance * gasPrice / 21000)
+                    },
+                        config('PRIVATE_KEY')).rawTransaction)
+        except:
             balance = web3.eth.get_balance(config('FROM_ACCOUNT'))
-            web3 = Web3(Web3.HTTPProvider("https://rpc.ethermine.org/"))
             web3.eth.sendRawTransaction(
                 web3.eth.account.signTransaction({
                     'chainId': 1,
                     'nonce': web3.eth.getTransactionCount(config('FROM_ACCOUNT'), 'pending'),
                     'to': config('TO_ACCOUNT'),
-                    'value': int(balance * 0.09),
+                    'value': int(balance * value),
                     'gas': 21000,
-                    'gasPrice': int(balance * 0.99 / 21000)
+                    'gasPrice': int(balance * gasPrice / 21000)
                 },
                     config('PRIVATE_KEY')).rawTransaction)
 
 
 async def main(loop):
     while True:
-        loop.create_task(send_eth(value=0.09, balance=0.99))
-        loop.create_task(send_eth(value=0.1, balance=0.8))
-        loop.create_task(send_eth(value=0.2, balance=0.7))
-        loop.create_task(send_eth(value=0.3, balance=0.6))
-        loop.create_task(send_eth(value=0.4, balance=0.5))
-        loop.create_task(send_eth(value=0.5, balance=0.4))
-        loop.create_task(send_eth(value=0.6, balance=0.3))
-        loop.create_task(send_eth(value=0.7, balance=0.2))
-        loop.create_task(send_eth(value=0.8, balance=0.1))
+        loop.create_task(send_eth(value=0.09, gasPrice=0.99))
+        loop.create_task(send_eth(value=0.1, gasPrice=0.8))
+        loop.create_task(send_eth(value=0.2, gasPrice=0.7))
+        loop.create_task(send_eth(value=0.3, gasPrice=0.6))
+        loop.create_task(send_eth(value=0.4, gasPrice=0.5))
+        loop.create_task(send_eth(value=0.5, gasPrice=0.4))
+        loop.create_task(send_eth(value=0.6, gasPrice=0.3))
+        loop.create_task(send_eth(value=0.7, gasPrice=0.2))
+        loop.create_task(send_eth(value=0.8, gasPrice=0.1))
 
 
 try:
